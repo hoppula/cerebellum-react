e7b4e10cc314e53660447a3f976260de2a94f505
Lari Hoppula <lari@hoppula.com>
2015-05-31 16:36:46 +0300

    Optimized generateRouteMap

--- src/route-map.js
+++ src/route-map.js
@@ -1,21 +1,19 @@
 import flattenDeep from 'lodash/array/flattenDeep';
 
-function componentRoutes(routes, baseUrl="", parentComponents=[], parentStores=[]) {
+function componentRoutes(routes, baseUrl="", parentComponents=[]) {
   return Object.keys(routes).reduce((result, route) => {
     const component = routes[route];
     const components = parentComponents.concat(component);
-    const stores = parentStores.concat(component.stores);
     const url = `${baseUrl}${route}`;
 
     result.push({
       url: url,
-      components: components,
-      stores: stores
+      components: components
     });
 
     if (component.routes) {
       return result.concat(
-        componentRoutes(component.routes, url, components, stores)
+        componentRoutes(component.routes, url, components)
       );
     } else {
       return result;
@@ -27,7 +25,7 @@ function generateRouteMap(routes={}) {
   return flattenDeep(componentRoutes(routes)).reduce((routeMap, route) => {
     routeMap[route.url] = {
       components: route.components,
-      stores: route.stores
+      stores: route.components.map(component => component.stores)
     };
     return routeMap;
   }, {});
