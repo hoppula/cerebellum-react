eb4b99c52b088139ae7c08511f38b59d2f2bee68
Lari Hoppula <lari@hoppula.com>
2015-06-02 19:51:53 +0300

    Added basic tests for route handler

---
+++ test/test_route-handler-nested.js
@@ -0,0 +1,29 @@
+import assert from 'assert';
+import routeHandler from '../route-handler-nested';
+
+const routeComponent = {
+  components: [],
+  stores: []
+};
+
+function handler(params) {
+  return params;
+}
+
+function context() {
+  return this;
+}
+
+describe('Route handler', () => {
+  it('should return passed handler for components with .components and .stores', () => {
+    assert.equal(routeHandler(routeComponent), routeComponent);
+  });
+
+  it('should call passed handler function with params for other components', () => {
+    assert.equal(routeHandler(handler, [123]), 123);
+  });
+
+  it('should retain route handler\'s this context', () => {
+    assert.deepEqual(routeHandler.call({title: "value"}, context), {title: "value"});
+  });
+});
